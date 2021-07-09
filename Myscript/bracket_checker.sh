ddf96a6ab62b50a85437431dd4a4f8f117f6c398
Ali Ustunkol <64160260+austunkol@users.noreply.github.com>
2021-07-09 12:37:56 -0400

    updates

---
+++ Myscript/bracket_checker.sh
@@ -0,0 +1,19 @@
+read -p "Write a bracket combination please: " bracket
+if [[ "$bracket" == "()" ]]
+then
+        echo "true"
+elif [[ "$bracket" == '""' ]]
+then
+        echo "true"
+elif [[ "$bracket" == "{}" ]]
+then
+        echo "true"
+elif [[ "$bracket" == "[]" ]]
+then
+        echo "true"
+elif [[ "$bracket" == "()[]{}" ]]
+then
+        echo "true"
+else
+        echo "false"
+fi
